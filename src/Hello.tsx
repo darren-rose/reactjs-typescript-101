a45bc93336780a9ad5ba9a29a23fd3f500436f0f
darren-rose <darrenwrose@gmail.com>
2020-02-11 22:03:31 +0100

    ic

---
+++ src/Hello.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+}
+
+function Hello({ name, enthusiasmLevel = 1 }: Props) {
+  if (enthusiasmLevel <= 0) {
+    return (
+      <div className="hello">
+        <div className="greeting">
+          What's up {name + getExclamationMarks(enthusiasmLevel)}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+    </div>
+  );
+}
+
+export default Hello;
+
+// helpers
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
